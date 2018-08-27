5514848af025db6e18cdbd43dd445ee4acf20ba6
Diego Rubin <rubin.diego@gmail.com>
2018-08-27 07:39:12 -0300

    working with reports.

--- public/js/reports.js
+++ public/js/reports.js
@@ -4,5 +4,30 @@ var Report = function() {
 Report.prototype.init = function(application) {
   var _this = this;
   _this.application = application;
+  _this.loadMeetingsReport();
+};
+
+Report.prototype.loadMeetingsReport = function() {
+  var _this = this;
+  $.ajax({
+    method: 'get', 
+    url: '/secure/reports/meetings',
+    dataType: 'json',
+    contentType: 'application/json',
+    headers: {
+      'Authorization': 'JWT ' + localStorage.getItem('access_token')
+    }
+  }).done(function(result) {
+    _this.application.render('reports/meetings', function() {
+    }, {
+        data: { result: result},
+        destination: '#mainContent'
+      });
+  }).fail(function(data){
+    console.log(data);
+    if (data.status == 401) {
+      _this.application.clearSession();
+    }
+  });
 };
 
--- reports/usecases/meetings.py
+++ reports/usecases/meetings.py
@@ -3,21 +3,37 @@ from groups.domains.meeting import Meeting
 
 
 def prepare(start, end):
-    result = {}
+    result = {
+        'groups': [],
+        'meetings': {}
+    }
+
+    for group in Group.query.all():
+        result['groups'].append({
+            'name': group.name,
+            'id': group.id
+        })
 
     meetings = (Meeting.query
                 .filter(Meeting.date >= start)
                 .filter(Meeting.date <= end)).all()
 
+    initial_week_number = start.isocalendar()[1]
+
+    result['weeks'] = list(range(1, end.isocalendar()[1] - initial_week_number))
     for meeting in meetings:
-        week_number = meeting.date.isocalendar()[1]
-        if week_number not in result:
-            result[week_number] = []
-        result[week_number].append({
-            'group_name': Group.query.filter(Group.id == meeting.group_id).first().name,
+        week_number = meeting.date.isocalendar()[1] - initial_week_number + 1
+        if meeting.group_id not in result['meetings']:
+            result['meetings'][meeting.group_id] = {}
+
+        if week_number not in result['meetings'][meeting.group_id]:
+            result['meetings'][meeting.group_id][week_number] = {}
+
+        result['meetings'][meeting.group_id][week_number] = {
+            'group_id': meeting.group_id,
             'number_of_children': meeting.number_of_children or 0,
             'number_of_participants': meeting.number_of_participants or 0,
             'number_of_visitors': meeting.number_of_visitors or 0
-        })
+        }
 
     return result
